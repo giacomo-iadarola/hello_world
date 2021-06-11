b471eb8334dc71d7967a8ab3960231075b73878b
giachi.iada <giachi.iada@gmail.com>
2021-06-11 12:28:41 +0200

    modifica2

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,6 +1,6 @@
 class HelloWorld2 {
     public static void main(String[] args) {
-        // il mio commento
+        // il mio commento secondo
         System.out.println("Hello, World!"); 
     }
 }
